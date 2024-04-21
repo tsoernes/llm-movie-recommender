317a75b23357137a47eeb1bef7c4078abd1e167e
Torstein Sørnes <t.soernes@gmail.com>
2024-04-21 21:53:44 +0200

    Use TMDb API directly

--- main.py
+++ main.py
@@ -1,28 +1,46 @@
 from config import TMDB_API_KEY
 import requests
 import polars as pl
-import numpy as np
-from bs4 import BeautifulSoup
-from tmdbv3api import TMDb, Discover, Genre
 
-tmdb = TMDb()
-tmdb.api_key = TMDB_API_KEY
-discover = Discover()
-movies = discover.discover_movies(
-    {
-        "sort_by": "vote_average.desc",
-        "vote_count.gte": 10_000,
-    }
-)
 
-id_and_genre = Genre().movie_list()["genres"]
+# Set the API endpoint and parameters
+url = "https://api.themoviedb.org/3/discover/movie"
+params = {
+    "api_key": TMDB_API_KEY,
+    "sort_by": "vote_average.desc",
+    "vote_count.gte": 10_000,
+    "page": 1,
+}
+
+# Send a GET request to the API
+response = requests.get(url, params=params)
+
+# Check if the request was successful
+if response.status_code != 200:
+    print("Failed to retrieve movies:", response.status_code)
+
+# Parse the JSON response
+data = response.json()
+
+# Extract the movie data from the response
+movies = data["results"]
+
+# Create a Polars DataFrame from the movie data
+df = pl.DataFrame(movies)
+
+# Fetch genre names
+url = "https://api.themoviedb.org/3/genre/movie/list"
+params = {"api_key": TMDB_API_KEY}
+response = requests.get(url, params=params)
+if response.status_code != 200:
+    print("Failed to retrieve movies:", response.status_code)
+data = response.json()
+id_and_genre = data["genres"]
+# Map genre ids to genre names
 id_to_genre = {x["id"]: x["name"] for x in id_and_genre}
 
-df = pl.from_dicts([dict(movie) for movie in movies])
 # Drop unecessary columns
 df = df[["id", "title", "overview", "genre_ids"]]
-# Convert from tmdb object dtype to list
-df = df.with_columns(df["genre_ids"].map_elements(list, return_dtype=list))
 # Add genre names
 df = df.with_columns(
     df["genre_ids"]
